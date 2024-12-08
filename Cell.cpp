cfa6833bef6a5a4b55e45899c5f29c63ae0d406a
LLerouxdev <louis.leroux@viacesi.fr>
2024-12-08 14:56:36 +0100

    Update Cell.cpp

--- Cell.cpp
+++ Cell.cpp
@@ -1,17 +1,19 @@
 #include "Cell.h"
 
+// Constructeur par défaut : initialise la cellule comme morte
 Cell::Cell(){
-    this->vivante = false;
+    this->vivante = false; // Par défaut, la cellule est morte
 }
-//implémentation du constructeur paramétré
+// Constructeur paramétré : initialise la cellule avec un état donné
 Cell::Cell(bool etat){ 
-    this->vivante = etat;
+    this->vivante = etat; // Initialise l'état de la cellule
 }
 
 // implémentation du getter qui retourne la variable vivante
 bool Cell::getVie() const {
     return vivante;
 }
+
 
 // implémentation du setter pour définir l'état de la cellule en bool
 void Cell::setEtat(bool etat){
